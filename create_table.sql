ee13c8678a9285480779c73db00161d9432dac69
cindy-wang328 <h687wang@uwaterloo.ca>
2020-06-04 22:52:39 -0400

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE myanimelist
   ( 
-     title    VARCHAR(100) NOT NULL PRIMARY KEY, 
+     title    VARCHAR(255) NOT NULL PRIMARY KEY, 
      rating     DECIMAL(6,0), 
      popularity    DECIMAL(6,0), 
-     score    DECIMAL(2,2), 
+     score    DECIMAL(4,2), 
      status      VARCHAR(50),
      premiered   VARCHAR(20)
   );
