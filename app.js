6f527622380240b4f5ef9571823f7e50d138d6cb
Tony Meng <tonym321321@gmail.com>
2020-06-20 23:58:20 -0600

    changed interface (in /anime_list)

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require("express");
 const mysql = require("promise-mysql");
 const bodyParser = require("body-parser"); // parsing form requests
 const nunjucks = require("nunjucks"); // templating engine
+const utils = require("./utils");
 
 const config = JSON.parse(fs.readFileSync(fs.existsSync("config-local.json") ? "config-local.json" : "config.json"));
 
@@ -23,8 +24,8 @@ let pool;
 
 app.get("/", async (req, res) => {
 	try {
-		const queryRes = await pool.query("SELECT * FROM myanimelist");
-		res.render("index.html", {waifuList: JSON.stringify(queryRes)});
+		const queryRes = await pool.query("SELECT * FROM anime");
+		res.render("index.html", {waifuList: queryRes});
 	} catch(err) {
 		console.log(err);
 		res.status(500).send("database failed").end();
@@ -39,6 +40,47 @@ app.post("/add", async (req, res) => {
 		console.log(err);
 	}
 	res.redirect("/");
+});
+
+let anime_list_request = async (req, res) => {
+	let acceptedSortFields = {
+		"title": "title_eng",
+		"rating": "score_mal"
+	};
+	let acceptedSortOrder = {
+		"ascending": "ASC",
+		"descending": "DESC"
+	};
+	
+	let query = `
+		SELECT
+			{fields}
+		FROM anime
+		ORDER BY {sort_field} {sort_order}
+		LIMIT 50
+	`;
+	query = utils.format_query(query, {
+		"fields": "title_eng, title_native, score_mal, type, img",
+		"sort_field": acceptedSortFields[req.body["sort_field"]],
+		"sort_order": acceptedSortOrder[req.body["sort_order"]]
+	});
+	console.log(query)
+	const queryRes = await pool.query(query);
+	// console.log(queryRes)
+
+	try {
+		res.render("anime_list.html", {animeList: queryRes, defaults: req.body});
+	} catch(err) {
+		console.log(err);
+		res.status(500).send("database failed").end();
+	}
+}
+app.post("/anime_list", anime_list_request);
+app.get("/anime_list", async (req, res) => {
+	// Default values
+	req.body["sort_field"] = "title";
+	req.body["sort_order"] = "ascending";
+	await anime_list_request(req, res);
 });
 
 (async () => {
